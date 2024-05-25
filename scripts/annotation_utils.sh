b8f59c9514df6ef71e962c1aa756c6a2d59fdd96
ujjwalaananth <ujjwalaananth@gmail.com>
2024-05-25 15:17:36 -0700

    ntse_17_18 done

--- scripts/annotation_utils.sh
+++ scripts/annotation_utils.sh
@@ -1,6 +1,6 @@
 # python annotation_utils.py \
 # --mode create_metadata \
-# --paper_path "../ntse_17_18/Question Paper_Delhi NTSE Stage 1 2017-18 MAT_SAT_LANG.pdf" \
+# --paper_path "../ntse_17_18/QP_Uttar Pradesh NTSE Stage 1 2017-18 (SAT_MAT_LANG).pdf" \
 # --annotator_name ujjwalaa \
 # --datastore_path "../datastore"
 
